c443da1f178875eb0bde8c771c0a1d533f7cdd71
USUARIO <USUARIO@DESKTOP-Q5ALF4J>
2023-10-03 19:24:41 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prob04_Laboratorio07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa un número: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(); // Cambia de línea después de imprimir una fila de asteriscos.
+            }
+        }
+    }
+
+}
+    
+
